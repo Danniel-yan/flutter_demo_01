13ffdc5d642d6c626bb72fcf0b29144998af7009
yanshirong <yanshirong@shanda.com>
2024-01-18 22:06:42 +0800

    构造方法、异常等demo

--- lib/类_构造方法.dart
+++ lib/类_构造方法.dart
@@ -1,9 +1,59 @@
 import 'package:flutter_demo_01/point.dart';
 
-void main(){
-  int? m = Point(1,2).x;
-  print(m);
+class View{
+  View(int context, int attr);
 
- var n = Point.X(5);
- print(n.x);
+  // 重定向构造方法
+  View.a(int context):this(context, 0);
+}
+
+//
+class ImmutabelPoint{
+   final int? x;
+   final int? y;
+
+  //常量构造方法
+   const ImmutabelPoint(this.x, this.y);
+
+}
+
+void main(){
+  // int? m = Point(1,2).x;
+  // print(m);
+  //
+  // var n = Point.X(5);
+  // print(n.x);
+
+  // 常量构造方法
+  var p1 = const ImmutabelPoint(1, 2);
+  var p2 = const ImmutabelPoint(1, 2);
+  print(p1.hashCode == p2.hashCode); // true 表示为同一个对象；如果参数不同 则为两个对象
+
+  var p3 =  ImmutabelPoint(1, 2);
+  var p4 =  ImmutabelPoint(1, 2);
+  print(p3.hashCode == p4.hashCode); // false 表示非一个对象
+
+
+  print(Manager.get());
+
+}
+
+class Manager{
+  // 工厂构造方法 必须返回一个实例对象 dart最常用
+  factory Manager.get(){
+
+    return Children();
+  }
+
+  // 不需要必须返回一个实例对象
+  static Manager get2(){
+
+    return Children();
+  }
+
+  Manager();
+}
+
+class Children extends Manager{
+
 }
