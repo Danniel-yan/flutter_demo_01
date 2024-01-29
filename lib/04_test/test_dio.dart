864c9f41f2c6ab7945594c50ab80876eac599889
yanshirong <yanshirong@shanda.com>
2024-01-29 12:47:54 +0800

    demo

--- lib/04_test/test_dio.dart
+++ lib/04_test/test_dio.dart
@@ -5,5 +5,7 @@ void main() async{
   var dio = Dio(); //相当于httpClient
   Response mResponse = await dio.get("https://www.wanandroid.com/banner/json");
   print(mResponse.data);
-  // 1：29：53 播放到了43分钟
+  // 1：29：53 播放到了43分钟  58分钟
+
 }
+
